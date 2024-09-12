bfae3177eac943e3434482d63a2d45244cfcfe9e
Fabian Meerveld <144360059+FabianMeerveld@users.noreply.github.com>
2024-09-12 23:56:40 +0200

    Update voor system PATH

--- idf.bat
+++ idf.bat
@@ -1,5 +1,11 @@
 @echo off
 
+REM Store the current directory
+SET "ORIGINAL_DIR=%CD%"
+
+REM Change to the directory where the batch file is located
+CD /D %~dp0
+
 REM Check if ..\esp-idf-v5.3 directory exists
 if exist ..\esp-idf-v5.3 (
     REM Call the export.bat script in the esp-idf-v5.3 directory
@@ -18,3 +24,6 @@ D:
 
 REM Open a new command prompt window
 cmd /k
+
+REM Return to the original directory
+CD /D "%ORIGINAL_DIR%"
